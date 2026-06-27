146fec7879f3b6f66de9fd02fb839997dd80743a
samir-magdy <samir.magdy.dev@protonmail.com>
2026-06-28 00:21:49 +0300

    absolute position the title on mobile

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -88,7 +88,7 @@ export default function HeroSection() {
       <div className="absolute inset-0 bg-black/40 pointer-events-none" />
       <div className="absolute inset-x-0 bottom-0 h-2/3 bg-linear-to-t from-black/80 to-transparent pointer-events-none" />
       <div className="absolute inset-0 bottom-0 flex items-center justify-between pointer-events-none">
-        <div className="leading-[1.05] ps-6 sm:ps-16 absolute bottom-16 sm:bottom-20">
+        <div className="leading-[1.05] ps-6 sm:ps-16 absolute bottom-20">
           <p className="font-bold text-white mb-6 sm:mb-12 text-[2.5rem] lg:text-[6vw]">
             Butter.
           </p>
