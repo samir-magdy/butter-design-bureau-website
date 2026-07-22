15000ff9a116c61bfa581683b8aa6bc998792d87
samir-magdy <samir.magdy.dev@protonmail.com>
2026-07-22 04:02:21 +0300

    credit

--- app/components/SiteByCredit.tsx
+++ app/components/SiteByCredit.tsx
@@ -7,12 +7,12 @@ export default function SiteByCredit() {
         Site by
       </span>
       <a
-        href="https://smwebdesign.studio"
+        href="https://smweb.studio"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center gap-0.5 sm:text-[0.6rem] sm:tracking-[0.15em] text-[0.55rem] uppercase tracking-widest decoration-[0.5px] hover:decoration-1 text-neutral-800 underline underline-offset-2 decoration-neutral-600 hover:text-black hover:decoration-black transition-colors duration-200"
       >
-        SM Web Design Studio
+        SM Web Studio
         <LuArrowUpRight className="w-2 h-2 sm:w-2.5 sm:h-2.5" />
       </a>
     </footer>
