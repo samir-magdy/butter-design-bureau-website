e8be4e746fd93130d1d849f71182bbe3b84f2267
samir-magdy <samir.magdy.dev@pm.me>
2026-07-31 09:59:42 +0300

    move to cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.29'],
+  // Cloudelfare Pages requires the following prop
+  output: "export"
 };
 
 export default nextConfig;
